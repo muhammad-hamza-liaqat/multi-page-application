303d4821a610b31076418d320cc7d0a836a2cc70
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-28 09:52:09 +0500

    basic routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Home() {
+    return (
+        <>
+            <div className="main">
+                <div className="home">
+                    <p>Hello, i am Home</p>
+                </div>
+            </div>
+        </>
+    )
+}
