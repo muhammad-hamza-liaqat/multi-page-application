d8d9da7693f062d08adfe3fb587903777499088e
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-10-29 09:33:12 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 // import './App.css'
 import { BrowserRouter as Router } from "react-router-dom"
 import MyAppRoutes from "./routes/myAppRoutes"
+import { CounterProvider } from "./providers/counterProvider"
 function App() {
 
   return (
     <>
-      <Router>
-        <MyAppRoutes />
-      </Router>
+      <CounterProvider>
+        <Router>
+          <MyAppRoutes />
+        </Router>
+      </CounterProvider>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
-import { StrictMode } from 'react'
+// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { CounterProvider } from './providers/counterProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <CounterProvider>
+  // <StrictMode>
       <App />
-    </CounterProvider>
-  </StrictMode>,
+  // </StrictMode>,
 )
