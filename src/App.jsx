224ce5134447434947e2f4e90c246876d759a32b
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-11-04 18:04:52 +0500

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
 // import './App.css'
-import { BrowserRouter as Router } from "react-router-dom"
-import MyAppRoutes from "./routes/myAppRoutes"
-import { CounterProvider } from "./providers/counterProvider"
 function App() {
 
   return (
     <>
-      <CounterProvider>
-        <Router>
-          <MyAppRoutes />
-        </Router>
-      </CounterProvider>
+
     </>
   )
 }
