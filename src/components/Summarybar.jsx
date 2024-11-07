c8094423b75bfafa0acbe9b20387f0a3d8468365
muhammad-hamza-liaqat <mh408800@gmail.com>
2024-11-07 16:51:07 +0500

    changes

--- src/components/Summarybar.jsx
+++ src/components/Summarybar.jsx
@@ -7,13 +7,13 @@ export default function Summarybar() {
 
     return (
         <>
-            <div className="main flex justify-center mt-5">
+            <div className="main flex justify-center mt-5 p-3">
                 <div className="w-full max-w-lg flex justify-between items-center">
                     <div>
                         Balance: {100} PKR
                     </div>
                     <div>
-                        <button className=' p-2 bg-slate-400 rounded-2xl' onClick={addExpense}>Add Expense</button>
+                        <button className='p-1 rounded-xl hover:bg-emerald-800 hover:text-white md:p-2 bg-slate-400 text-sm md:rounded-2xl md:hover:bg-gray-600' onClick={addExpense}>Add Expense</button>
                     </div>
                 </div>
             </div>
